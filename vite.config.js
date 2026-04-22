185e26ba5bd519d93ff4dbee8213309be5bc574a
Toxu <gonzasoft2023@gmail.com>
2026-04-22 22:06:12 +0200

    Configure GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: './',
+export default defineConfig(({ command }) => ({
+  base: command === 'serve' ? '/' : '/LaRomana/',
   plugins: [react()],
   optimizeDeps: {
     include: ['qrcode'],
   },
-})
+}))
